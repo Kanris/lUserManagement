39d45dbc42a3ba9a5693e0cfc671cec4089f7b1a
Alexey Glukhov <kanris2@gmail.com>
2016-08-01 14:08:17 +0300

    Add project to GitHub

---
+++ UserManagment.Tests/UserManagmentTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserManagment.Tests
+{
+    [TestClass]
+    public class UserManagmentTests
+    {
+        public TestContext TestContext { set; get; }
+        private UserManagment managment = new UserManagment();
+        
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "userData.xml",
+            "User",
+            DataAccessMethod.Sequential
+            )]
+        [TestMethod]
+        public void Add_User_FromXML()
+        {
+            string id = Convert.ToString(TestContext.DataRow["id"]);
+            string phone = Convert.ToString(TestContext.DataRow["phone"]);
+            string email = Convert.ToString(TestContext.DataRow["email"]);
+
+            bool result = managment.Add(id, phone, email);
+
+            Assert.IsTrue(result, "Can't add this user");
+        }
+    }
+}
---
+++ lUserManagment/UserManagment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagment
+{
+    public class UserManagment
+    {
+        public bool Add(string id, string phone, string email)
+        {
+            if (id.Length < 4)
+            {
+                throw new Exception("Can't add new user with id length less than 4");
+            }
+
+            if (!isPhoneNumber(phone))
+            {
+                throw new Exception("Can't add phone that do not match the pattern");
+            }
+
+            if(!isEmailAdress(email))
+            {
+                throw new Exception("Can't email");
+            }
+
+            return true;
+        }
+
+        private bool isPhoneNumber(string phone)
+        {
+            string pattern = @"\+38\d{10}";
+
+            return Regex.IsMatch(phone, pattern);
+        }
+
+        private bool isEmailAdress(string email)
+        {
+            string pattern = @"^[a-z_]{3,}@[a-z]{2,}\.[a-z]{2,}";
+
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+    }
+}
