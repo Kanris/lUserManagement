0781049e81c6c57417733b1ed96e667c3e0ced23
Alexey Glukhov <kanris2@gmail.com>
2016-08-25 12:51:26 +0300

    Add fail test

--- UserManagment.Tests/UserManagmentTests.cs
+++ UserManagment.Tests/UserManagmentTests.cs
@@ -10,7 +10,7 @@ namespace UserManagment.Tests
         private UserManagment managment = new UserManagment();
         
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
-            "userData.xml",
+            @"TestData\correctUserData.xml",
             "User",
             DataAccessMethod.Sequential
             )]
@@ -24,6 +24,21 @@ namespace UserManagment.Tests
             bool result = managment.Add(id, phone, email);
 
             Assert.IsTrue(result, "Can't add this user");
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            @"TestData\failUserData.xml",
+            "User",
+            DataAccessMethod.Sequential)]
+        [ExpectedException(typeof(Exception))]
+        [TestMethod]
+        public void Fail_Add_User_FromXML()
+        {
+            string id = Convert.ToString(TestContext.DataRow["id"]);
+            string phone = Convert.ToString(TestContext.DataRow["phone"]);
+            string email = Convert.ToString(TestContext.DataRow["email"]);
+
+            bool result = managment.Add(id, phone, email);
         }
     }
 }
